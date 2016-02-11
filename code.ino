6ba54cf65110924a421e685e27dd1a436f061c41
wcoder52 <weylan@bee-eee.com>
2016-02-11 15:45:58 -0700

    Create code.ino

---
+++ code.ino
@@ -0,0 +1,43 @@
+# NodeMcuV3_mesh
+#include <ESP8266WiFi.h>
+#include <ESP8266WiFiMesh.h>
+String manageRequest(String request);
+/* Create the mesh node object */
+ESP8266WiFiMesh mesh_node = ESP8266WiFiMesh(ESP.getChipId(), manageRequest);
+
+/**
+ * Callback for when other nodes send you data
+ *
+ * @request The string received from another node in the mesh
+ * @returns The string to send back to the other node
+ */
+String manageRequest(String request)
+{
+	// Print out received message 
+	Serial.print("received: ");
+	Serial.println(request);
+	// return a string to send back
+	return String("Hello world response1.");
+}
+
+void setup()
+{
+	Serial.begin(115200);
+	delay(10);
+	Serial.println();
+	Serial.println();
+	Serial.println("Setting up mesh node...");
+
+	/* Initialise the mesh node */
+	mesh_node.begin();
+}
+
+void loop()
+{
+	/* Accept any incoming connections */
+	mesh_node.acceptRequest();
+
+	/* Scan for other nodes and send them a message */
+	//mesh_node.attemptScan("Hello Tyler.");
+	delay(10);
+}
